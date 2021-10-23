d01703a057d05dabd037959beffae49a0b8cf2a1
alirrah <alir.rah81@gmail.com>
2021-10-23 15:18:02 +0330

    complete constrator

--- project.cpp
+++ project.cpp
@@ -3,6 +3,10 @@
 #include <vector>
 using namespace std;
 
+#define pause system("pause")
+
+int findNumber(string &, int &);
+
 class matrix
 {
 private:
@@ -11,15 +15,59 @@ private:
     public:
         pair<int, int> location;
         int value;
-        node *nextRow;
-        node *nextColumn;
+        node *nextRow, *nextColumn;
     };
-    vector<node *> headColumn;
-    vector<node *> headRow;
+    vector<node *> headColumn, headRow;
     int row, column;
 public:
     matrix(string &file)
     {
+        try
+        {
+            FILE *fptr;
+            char fileSentence;
+            int i, j;
+            string sent;
+            fptr = fopen(file.c_str(), "r");
+            if (fptr == NULL)
+                throw "Error!!! file could not be opened";
+            for(i = 0; i < file.size(); i++)
+                if(file[i] == 'M' && file[i + 1] == '(')
+                    break;
+            i += 2;
+            row = findNumber(file, i);
+            i++;
+            column = findNumber(file, i);
+            headRow.resize(row);
+            headColumn.resize(column);
+            i = 1;
+            j = 1;
+            while((fileSentence = fgetc(fptr)) != EOF)
+            {
+                if(isdigit(fileSentence))
+                    sent += fileSentence;
+                else if(fileSentence == ',' || fileSentence == '\n')
+                {
+                    insert(i, j, stoi(sent));
+                    sent.clear();
+                    j++;
+                    if(j > column)
+                    {
+                        j = 1;
+                        i++;
+                    }
+                }
+
+            }
+            fclose(fptr);
+        }
+        catch (char const *message)
+        {
+            cout << message << endl;
+            pause;
+            exit(1);
+        }
+
     }
     ~matrix()
     {
@@ -47,4 +95,13 @@ public:
 int main()
 {
     return 0;
+}
+
+int findNumber(string &sentence, int &j)
+{
+    int i = j;
+    for(; j < sentence.size(); j++)
+        if(!isdigit(sentence[j]))
+            break;
+    return stoi(sentence.substr(i, j));
 }
